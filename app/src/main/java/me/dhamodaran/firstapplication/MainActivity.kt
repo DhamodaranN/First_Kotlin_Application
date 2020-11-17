5eba602a18d6c1e2d1215c87665714c64f41e826
Dhamodaran N <dhamodaran.net@gmail.com>
2020-11-17 12:13:07 +0530

    Initial commit

---
+++ app/src/main/java/me/dhamodaran/firstapplication/MainActivity.kt
@@ -0,0 +1,19 @@
+package me.dhamodaran.firstapplication
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+    fun change(view: View) {
+        imageView.setImageResource(R.drawable.tomj2)
+    }
+}
